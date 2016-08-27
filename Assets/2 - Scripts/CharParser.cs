6acd93beacbe01aa222a9bc4e7a7efe848aa311b
YuhanKim <yuhankim@maverickgames.co>
2016-08-28 03:21:36 +0900

    Mod: Update New data (Takumi Mukai)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[80];
+        _charData = new string[81];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
