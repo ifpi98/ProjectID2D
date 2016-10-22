fc9e3ac4b1966403056e728c7fedc82b597bea0f
YuhanKim <yuhankim@maverickgames.co>
2016-10-22 16:59:46 +0900

    Mod: Update New data (Shiki Ichinose and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[84];
+        _charData = new string[85];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
