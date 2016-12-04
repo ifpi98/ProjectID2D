1e878fee81542bbd085469b5f2eb58ec87f5bf1d
yuhankim <yuhankim@maverickgames.co>
2016-12-05 02:57:42 +0900

    Mod: Update New data (Karin Domyoji and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[87];
+        _charData = new string[88];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
--- Assets/2 - Scripts/UnitParser.cs
+++ Assets/2 - Scripts/UnitParser.cs
@@ -35,7 +35,7 @@ public class UnitParser : MonoBehaviour
 
     public void Parse()
     {
-        _unitData = new string[292];
+        _unitData = new string[300];
 
 
 
