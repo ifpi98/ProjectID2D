d17f9a6b8b7f04358abc807dadfe9eb56a439d32
yuhankim <yuhankim@maverickgames.co>
2017-01-16 22:06:43 +0900

    Mod: Update New data (Ryo Matsunaga and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[90];
+        _charData = new string[91];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
--- Assets/2 - Scripts/UnitParser.cs
+++ Assets/2 - Scripts/UnitParser.cs
@@ -35,7 +35,7 @@ public class UnitParser : MonoBehaviour
 
     public void Parse()
     {
-        _unitData = new string[312];
+        _unitData = new string[317];
 
 
 
