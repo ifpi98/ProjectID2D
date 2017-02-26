77ce4471e9f5a8944e6dc18238baa630f424fea3
yuhankim <yuhankim@maverickgames.co>
2017-02-26 23:39:48 +0900

    Mod: Update New data (Shin Satoh and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[92];
+        _charData = new string[93];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
