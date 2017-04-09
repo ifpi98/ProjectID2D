bf25eb06da9eb11887fe4a6ee202a5a1f65cfffc
yuhankim <yuhankim@maverickgames.co>
2017-04-10 00:36:13 +0900

    Mod: Update New data (Hajime Fujiwara and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[94];
+        _charData = new string[95];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
