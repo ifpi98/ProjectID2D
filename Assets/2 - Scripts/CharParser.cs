c209f693591b02f28d9f29f31802efcc506c6d50
yuhankim <yuhankim@maverickgames.co>
2017-05-29 00:22:25 +0900

    Mod: Update New data (Kako Takafuji and New Units)

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -36,7 +36,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[96];
+        _charData = new string[97];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
